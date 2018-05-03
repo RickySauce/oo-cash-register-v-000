cd789494373efbfc5cb529358b73c001306698e5
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-03 13:43:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end
 
   def apply_discount
-    discount_total = self.total * .@discount
+    discount_total = self.total * @discount
     @total = self.total - discount_total
   end
 
